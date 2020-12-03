0e8def6a90db3a684b141bdfa25714ee8a4a1293
alan <yyl_email@126,com>
2020-12-04 00:22:29 +0800

    代码清单3-12:自定义第三方bean

--- chapter3/src/main/java/com/springboot/chapter3/config/AppConfig.java
+++ chapter3/src/main/java/com/springboot/chapter3/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.springboot.chapter3.config;
 
 import com.springboot.chapter3.pojo.*;
-import org.springframework.context.annotation.ComponentScan;
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 
 /**
@@ -17,13 +20,13 @@ import org.springframework.stereotype.Service;
  * @date 2020/12/3 19:25
  */
 @Configuration
-@ComponentScan(basePackages = "com.springboot.chapter3.*",excludeFilters = {@ComponentScan.Filter(classes = {Service.class})})
 public class AppConfig {
 
     /**
      * 指定了name的值，将initUser方法返回的POJO装配到IoC容器中
      * @return
      */
+    @Bean("user")
     public User initUser(){
         User user = new User();
         // 1L表示1是长整型
@@ -32,6 +35,31 @@ public class AppConfig {
         user.setNote("note_1");
 
         return user;
+    }
+
+
+    /**
+     * @Bean注解实现将getDataSource()方法的返回值注入到IoC容器中
+     * @return
+     */
+    @Bean("dataSource")
+    public DataSource getDataSource(){
+        Properties props = new Properties();
+
+        props.setProperty("driver","com.mysql.jdbc.Driver");
+        props.setProperty("url","jdbc:mysql://localhost:3306/chapter3");
+        props.setProperty("username","root");
+        props.setProperty("password","123456");
+
+        DataSource dataSource=null;
+
+        try {
+            dataSource= BasicDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return dataSource;
     }
 
 }
